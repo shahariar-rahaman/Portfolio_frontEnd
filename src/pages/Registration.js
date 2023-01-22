6e3a714fb7dce7a73d84087ea11163255580272f
ridoy2112 <you@example.com>
2023-01-22 21:25:37 +0600

    Six commit

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -11,7 +11,7 @@ const Registration = () => {
     const [emailErr,setEmailErr] = useState("")
     const [passwordErr,setPasswordErr] = useState("")
     const [confirmPasswordErr,setConfirmPasswordErr] = useState("")
-    
+    const url=process.env.REACT_APP_BASE_URL
     const navigator = useNavigate()
     
     const handleSubmit=(e)=>{
@@ -31,7 +31,7 @@ const Registration = () => {
         }
         else{
                async function admin(){
-                        await axios.post("http://localhost:8000/registration",{
+                        await axios.post(`${url}/registration`,{
                             name:name,
                             email:email,
                             password:password,
