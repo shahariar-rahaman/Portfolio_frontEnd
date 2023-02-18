7fe8b955955b3cabd2ff07e3b3b477706f9ab9c5
ridoy2112 <you@example.com>
2023-02-18 22:20:04 +0600

    X commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -17,7 +17,7 @@ const Banner = () => {
       <div className="itemSize">
         <img
           className="d-block w-100 size"
-          src="https://res.cloudinary.com/drzqify5h/image/upload/v1676736564/IMG_20220331_132551_gv3ery.jpg"
+          src="https://res.cloudinary.com/drzqify5h/image/upload/v1676737173/IMG_20211012_092432_1_nezvra.jpg"
           alt="Second slide"
         />
         </div>
